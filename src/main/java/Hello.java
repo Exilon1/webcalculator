32731fb3856dd6053e3f75d78f28333a63367da0
Alexey <delexyz@mail.ru>
2017-01-20 12:44:13 +0400

    Inicial

---
+++ src/main/java/Hello.java
@@ -0,0 +1,15 @@
+package main.java;
+
+/**
+ * Created by Dima on 17.01.2017.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public String printHello() {
+        return "Hellooooo!!!!";
+    }
+}
